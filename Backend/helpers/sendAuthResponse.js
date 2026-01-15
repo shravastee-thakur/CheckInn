1af43adf5a986fae738b5b766be471d36b0a4fc0
Shravastee Thakur <shravastee@gmail.com>
2026-01-15 17:16:44 +0530

    sameSite changes to none

--- Backend/helpers/sendAuthResponse.js
+++ Backend/helpers/sendAuthResponse.js
@@ -3,7 +3,7 @@ export const sendAuthResponse = (res, tokens, user, message = "Success") => {
     .cookie("refreshToken", tokens.refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     .status(200)
